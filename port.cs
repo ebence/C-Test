9c5ee2ecc85efedc032c5e59bfc28d7c7f275023
Bence Erdős <bence.erdos@proinvento.hu>
2010-10-23 17:03:02 +0200

    Revert "asd"
    This reverts commit 12aa222ee12ef7c0c333da86346b2afe5ec592e5.

---
+++ port.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestToBeDeleted100804
+{
+    class port:System.IO.Ports.SerialPort
+    {
+        /// <summary>
+        /// Elsõ módosítás
+
+        /// </summary>
+        public port() { }
+    }
+    class myTrial:System.IO.Ports.SerialPort
+    {
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+    }
+
+    class DerivedTrial : myTrial
+    {
+        private int myInt;
+
+        public int DerivedInt
+        {
+            get { return myInt; }
+            set { myInt = value; }
+        }
+
+    }
+    class x
+    {
+        public void s()
+        {
+            myTrial trial = new myTrial();
+            trial.MyProperty = 1;
+            
+            DerivedTrial derived = new DerivedTrial();
+            derived.MyProperty = 1;
+
+        }
+    }
+}
